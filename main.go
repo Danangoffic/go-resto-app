80e06b93ba6db39c4f8a7e4096fcbad612452141
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-03-06 00:12:53 +0700

    add concurrency using waitgroup, mutex as async

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-func jalanTugas() {
-
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("recover from: ", r)
-		}
-	}()
-
-	fmt.Println("menjalankan tugas")
-	panic("Panic panic panic")
+func testPrint(id int) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("[%d] counting %d\n", id, i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
 }
 
 func main() {
-	jalanTugas()
-	fmt.Println("sudah selesai")
+	var sharedResource string
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			mutex.Lock()
+			fmt.Println("Previous value: ", sharedResource)
+			sharedResource = fmt.Sprintf("key owned by: [%d]", id)
+			fmt.Println("Current value: ", sharedResource)
+			mutex.Unlock()
+			// testPrint(i)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("final resourc: ", sharedResource)
 }
