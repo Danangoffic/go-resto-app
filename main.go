414a90d93d0c8f0345d0f20ad655e0a8a9249688
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-03-06 02:24:27 +0700

    add channel with go routines

--- main.go
+++ main.go
@@ -15,7 +15,62 @@ func testPrint(id int) {
 	}
 }
 
+func greet(c chan string) {
+	name := <-c // set name as a channel
+	fmt.Println("Hello ", name)
+}
+
+func greetUntilOut(c chan string, out chan int) {
+	for {
+		select {
+		case name := <-c:
+			fmt.Println("Hello ", name)
+		case <-out:
+			fmt.Println("Ext greeter")
+			return
+		}
+	}
+}
+
+func nameReceiver(c chan string, out chan int) {
+	for {
+		select {
+		case name, more := <-c:
+			if more {
+				fmt.Println("Hello ", name)
+			} else {
+				fmt.Println("Received all data")
+				out <- 0
+			}
+
+		}
+	}
+}
+
+func nameProducer(c chan string) {
+	c <- "World" // set channel to "World" string
+	c <- "Banana"
+	c <- "Apple"
+}
+
 func main() {
+	c := make(chan string) // channel dengan string
+	out := make(chan int)  // channel dengan tipe int
+	// go greet(c)
+	// go greetUntilOut(c, out)
+	go nameReceiver(c, out)
+	nameProducer(c)
+	close(c)
+	// c <- "World" // set channel to "World" string
+	// c <- "Banana"
+	// c <- "Apple"
+
+	<-out
+	// out <- 0 // greeter exit
+	// time.Sleep(1 * time.Second)
+}
+
+func goroutines() {
 	var sharedResource string
 	var mutex sync.Mutex
 	var wg sync.WaitGroup
